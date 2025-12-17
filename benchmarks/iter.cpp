536e8a972a259547ad50e1a7372d82daf3d744ad
Guntram Strecha <6010606+GuntramStrecha@users.noreply.github.com>
2025-12-17 12:26:14 +0100

    Add iterator benchmarks for different container sizes

--- benchmarks/iter.cpp
+++ benchmarks/iter.cpp
@@ -15,7 +15,7 @@
 #include <ranges>
 #include <vector>
 
-TEST_CASE("Benchmark ZipView Bidirectional Containers", "[bidirectional]")
+TEST_CASE("Benchmark ZipView Bidirectional Same Sizes", "[bidirectional][same_sizes]")
 {
   std::list<int> l1(100000);
   std::list<int> l2(100000);
@@ -27,14 +27,14 @@ TEST_CASE("Benchmark ZipView Bidirectional Containers", "[bidirectional]")
   auto zip_ref = std::ranges::views::zip(l1, l2);
   auto zip_sut = gst::ranges::views::zip(l1, l2);
 
-  BENCHMARK("std zip bidi iterate")
+  BENCHMARK("std zip bidi same sizes")
   {
     int sum = 0;
     for (auto t : zip_ref) { sum += std::get<0>(t) + std::get<1>(t); }
     return sum;
   };
 
-  BENCHMARK("gst zip bidi iterate")
+  BENCHMARK("gst zip bidi same sizes")
   {
     int sum = 0;
     for (auto t : zip_sut) { sum += std::get<0>(t) + std::get<1>(t); }
@@ -42,7 +42,7 @@ TEST_CASE("Benchmark ZipView Bidirectional Containers", "[bidirectional]")
   };
 }
 
-TEST_CASE("Benchmark ZipView Forward Iteration", "[forward]")
+TEST_CASE("Benchmark ZipView Forward Same Sizes", "[forward][same_sizes]")
 {
   std::vector<int> v1(100000);
   std::vector<int> v2(100000);
@@ -53,7 +53,7 @@ TEST_CASE("Benchmark ZipView Forward Iteration", "[forward]")
   auto zip_ref = std::ranges::views::zip(v1, v2);
   auto zip_sut = gst::ranges::views::zip(v1, v2);
 
-  BENCHMARK("std zip fwd iterate")
+  BENCHMARK("std zip fwd same sizes")
   {
     int  sum = 0;
     auto it  = zip_ref.begin();
@@ -65,7 +65,7 @@ TEST_CASE("Benchmark ZipView Forward Iteration", "[forward]")
     return sum;
   };
 
-  BENCHMARK("gst zip fwd iterate")
+  BENCHMARK("gst zip fwd same sizes")
   {
     int  sum = 0;
     auto it  = zip_sut.begin();
@@ -78,7 +78,7 @@ TEST_CASE("Benchmark ZipView Forward Iteration", "[forward]")
   };
 }
 
-TEST_CASE("Benchmark ZipView Reverse Iteration", "[reverse]")
+TEST_CASE("Benchmark ZipView Reverse Same Sizes", "[reverse][same_sizes]")
 {
   std::vector<int> v1(100000);
   std::vector<int> v2(100000);
@@ -89,7 +89,7 @@ TEST_CASE("Benchmark ZipView Reverse Iteration", "[reverse]")
   auto zip_ref = std::ranges::views::zip(v1, v2);
   auto zip_sut = gst::ranges::views::zip(v1, v2);
 
-  BENCHMARK("std zip rev iterate")
+  BENCHMARK("std zip rev same sizes")
   {
     int  sum = 0;
     auto it  = zip_ref.end();
@@ -101,7 +101,105 @@ TEST_CASE("Benchmark ZipView Reverse Iteration", "[reverse]")
     return sum;
   };
 
-  BENCHMARK("gst zip rev iterate")
+  BENCHMARK("gst zip rev same sizes")
+  {
+    int  sum = 0;
+    auto it  = zip_sut.end();
+    while (it != zip_sut.begin())
+    {
+      --it;
+      sum += std::get<0>(*it) + std::get<1>(*it);
+    }
+    return sum;
+  };
+}
+
+TEST_CASE("Benchmark ZipView Bidirectional Different Sizes", "[bidirectional][different_sizes]")
+{
+  std::list<int> l1(100000);
+  std::list<int> l2(100001); // Different size
+
+  std::iota(l1.begin(), l1.end(), 0);
+  std::iota(l2.begin(), l2.end(), 100000);
+
+  auto zip_ref = std::ranges::views::zip(l1, l2);
+  auto zip_sut = gst::ranges::views::zip(l1, l2);
+
+  BENCHMARK("std zip bidi diff sizes")
+  {
+    int sum = 0;
+    for (auto t : zip_ref) { sum += std::get<0>(t) + std::get<1>(t); }
+    return sum;
+  };
+
+  BENCHMARK("gst zip bidi diff sizes")
+  {
+    int sum = 0;
+    for (auto t : zip_sut) { sum += std::get<0>(t) + std::get<1>(t); }
+    return sum;
+  };
+}
+
+TEST_CASE("Benchmark ZipView Forward Different Sizes", "[forward][different_sizes]")
+{
+  std::vector<int> v1(100000);
+  std::vector<int> v2(100001); // Different size
+
+  std::iota(v1.begin(), v1.end(), 0);
+  std::iota(v2.begin(), v2.end(), 100000);
+
+  auto zip_ref = std::ranges::views::zip(v1, v2);
+  auto zip_sut = gst::ranges::views::zip(v1, v2);
+
+  BENCHMARK("std zip fwd diff sizes")
+  {
+    int  sum = 0;
+    auto it  = zip_ref.begin();
+    while (it != zip_ref.end())
+    {
+      sum += std::get<0>(*it) + std::get<1>(*it);
+      ++it;
+    }
+    return sum;
+  };
+
+  BENCHMARK("gst zip fwd diff sizes")
+  {
+    int  sum = 0;
+    auto it  = zip_sut.begin();
+    while (it != zip_sut.end())
+    {
+      sum += std::get<0>(*it) + std::get<1>(*it);
+      ++it;
+    }
+    return sum;
+  };
+}
+
+TEST_CASE("Benchmark ZipView Reverse Different Sizes", "[reverse][different_sizes]")
+{
+  std::vector<int> v1(100000);
+  std::vector<int> v2(100001); // Different size
+
+  std::iota(v1.begin(), v1.end(), 0);
+  std::iota(v2.begin(), v2.end(), 100000);
+
+  auto zip_ref = std::ranges::views::zip(v1, v2);
+  auto zip_sut = gst::ranges::views::zip(v1, v2);
+
+  BENCHMARK("std zip rev diff sizes")
+  {
+    int  sum = 0;
+    auto it  = zip_ref.end();
+    while (it != zip_ref.begin())
+    {
+      --it;
+      sum += std::get<0>(*it) + std::get<1>(*it);
+    }
+    return sum;
+  };
+
+  BENCHMARK("gst zip rev diff sizes")
   {
     int  sum = 0;
     auto it  = zip_sut.end();
